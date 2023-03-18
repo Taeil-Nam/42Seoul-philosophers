6ed01c1989d1e299488d5c7c92f219b19473afe5
Taeil-Nam <nam0314@gmail.com>
2023-03-18 20:28:15 +0900

    Add - 멀티스레드 테스트
    - Undefined error : 0 원인 해결 필요.
    - #1

--- philo/srcs/philo_main.c
+++ philo/srcs/philo_main.c
@@ -3,17 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   philo_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:15:35 by tnam              #+#    #+#             */
-/*   Updated: 2023/03/16 17:33:19 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/03/18 20:23:08 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char *argv[])
+void	*thread_test(void *arg)
 {
-	argv = 0; // dummy code
+	int	thread_num;
+
+	thread_num = *(int *)arg;
+	printf("My thread ID = %d\n", thread_num);
+	pthread_exit(NULL);
+}
+
+void	error(void)
+{
+	perror(NULL);
+	exit(EXIT_FAILURE);
+}
+
+int	main(void)
+{
+	pthread_t	threads[5];
+	int			arg[5];
+	int			i;
+
+	i = 0;
+	while (i < 5)
+	{
+		arg[i] = i;
+		if (pthread_create(&(threads[i]), NULL, thread_test, &arg[i]) != 0)
+			error();
+		i++;
+	}
+	i = 0;
+	while (i < 5)
+	{
+		if (pthread_join(threads[i], NULL) != 0)
+			error();
+	}
 	return (0);
 }
