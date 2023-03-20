f80547c11153587dfafd27acbaa60132e7fbc61f
Taeil-Nam <nam0314@gmail.com>
2023-03-20 22:05:23 +0900

    Add - 스레드의 동기화 문제(race condition) 발생 코드
    - 스레드들이 함수를 호출하고 나면, 스레드간 공유하는 전역 변수의 값이 이상한 값으로 변한다.
    - 스레드 1은 전역 변수 num에 1을 50000번 더하고, 스레드 2는 전역 변수 num에 1을 50000번 빼는데 결과 값은 0이 아닌 다른 값이 나온다.
    - 스레드들이 context switching 되면서, 공유 자원에 대한 race condition 문제가 발생한다.
    - #1

--- philo/srcs/philo_main.c
+++ philo/srcs/philo_main.c
@@ -6,18 +6,52 @@
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:15:35 by tnam              #+#    #+#             */
-/*   Updated: 2023/03/20 21:37:50 by tnam             ###   ########.fr       */
+/*   Updated: 2023/03/20 22:04:55 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*thread_test(void *arg)
-{
-	int	thread_num;
+int	g_num = 0;
 
-	thread_num = *(int *)arg;
-	printf("My thread ID = %d\n", thread_num);
+void	*add(void *count);
+void	*sub(void *count);
+void	error(void);
+
+int	main(void)
+{
+	pthread_t	add_thread;
+	pthread_t	sub_thread;
+	int			count;
+
+	count = 50000;
+	if (pthread_create(&add_thread, NULL, add, &count) != 0
+		|| pthread_create(&sub_thread, NULL, sub, &count) != 0)
+		error();
+	if (pthread_join(add_thread, NULL) != 0
+		|| pthread_join(sub_thread, NULL) != 0)
+		error();
+	printf("result add() and sub() : %d\n", g_num);
+	return (0);
+}
+
+void	*add(void *count)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < *(int *)count)
+		g_num++;
+	return (EXIT_SUCCESS);
+}
+
+void	*sub(void *count)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < *(int *)count)
+		g_num--;
 	return (EXIT_SUCCESS);
 }
 
@@ -25,28 +59,4 @@ void	error(void)
 {
 	perror(NULL);
 	exit(EXIT_FAILURE);
-}
-
-int	main(void)
-{
-	pthread_t	threads[5];
-	int			arg[5];
-	int			i;
-
-	i = 0;
-	while (i < 5)
-	{
-		arg[i] = i;
-		if (pthread_create(&threads[i], NULL, thread_test, &arg[i]) != 0) // 새로운 스레드 생성 후, arg를 인자로 사용하여 thread_test 함수 실행.
-			error();
-		i++;
-	}
-	i = 0;
-	while (i < 5)
-	{
-		if (pthread_join(threads[i], NULL) != 0) // pthread_create()로 생성된 스레드들이 실행을 끝낼 때까지 대기.
-			error();
-		i++;
-	}
-	return (0);
 }
