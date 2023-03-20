80283217d94deb950c1c3ce8074e475e09744947
Taeil-Nam <nam0314@gmail.com>
2023-03-20 21:47:35 +0900

    Modify - Undefined error: 0 해결
    - pthread_exit() 대신 return 사용하니까 해결 됨.

--- philo/srcs/philo_main.c
+++ philo/srcs/philo_main.c
@@ -6,7 +6,7 @@
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:15:35 by tnam              #+#    #+#             */
-/*   Updated: 2023/03/18 20:23:08 by tnam             ###   ########.fr       */
+/*   Updated: 2023/03/20 21:37:50 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*thread_test(void *arg)
 
 	thread_num = *(int *)arg;
 	printf("My thread ID = %d\n", thread_num);
-	pthread_exit(NULL);
+	return (EXIT_SUCCESS);
 }
 
 void	error(void)
@@ -37,15 +37,16 @@ int	main(void)
 	while (i < 5)
 	{
 		arg[i] = i;
-		if (pthread_create(&(threads[i]), NULL, thread_test, &arg[i]) != 0)
+		if (pthread_create(&threads[i], NULL, thread_test, &arg[i]) != 0) // 새로운 스레드 생성 후, arg를 인자로 사용하여 thread_test 함수 실행.
 			error();
 		i++;
 	}
 	i = 0;
 	while (i < 5)
 	{
-		if (pthread_join(threads[i], NULL) != 0)
+		if (pthread_join(threads[i], NULL) != 0) // pthread_create()로 생성된 스레드들이 실행을 끝낼 때까지 대기.
 			error();
+		i++;
 	}
 	return (0);
 }
