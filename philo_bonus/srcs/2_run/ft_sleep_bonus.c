3a9008e397248726030cd8a6dbef49ef0494f27c
Taeil-Nam <tnam@student.42seoul.kr>
2023-04-13 15:58:41 +0900

    feat: 철학자간 실행 delay 최소화
    - ft_eat(), ft_sleep(), ft_monitoring_thread()의 while문에 usleep(100); 추가.
    
    Related to: #4

--- philo_bonus/srcs/2_run/ft_sleep_bonus.c
+++ philo_bonus/srcs/2_run/ft_sleep_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 19:08:14 by tnam              #+#    #+#             */
-/*   Updated: 2023/04/10 20:48:26 by tnam             ###   ########.fr       */
+/*   Updated: 2023/04/13 14:41:10 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	ft_sleeping(long start_time, t_philo *philo)
 {
 	printf("%ld %ld is sleeping\n", ft_current_time(philo), philo->philo_id);
 	while (ft_current_time(philo) - start_time < philo->time_to_sleep)
-		;
+		usleep(100);
 }
 
 void	ft_sleep(t_philo *philo)
